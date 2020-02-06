11f3b78f063a463fbd65136eaee9dc6f5a74085c
znixonz <znixonz@yahoo.com>
2020-02-07 00:09:17 +0800

    fixing the app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(
+      MaterialApp(
         home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Center(
-          child: Text("i am rich"),
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Center(
+              child: Text("i am rich"),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
         ),
-        backgroundColor: Colors.blueGrey[900],
       ),
-      body: Center(
-        child: Image(
-          image: AssetImage('images/diamond.png'),
-        ),
-      ),
-    )));
+    );
